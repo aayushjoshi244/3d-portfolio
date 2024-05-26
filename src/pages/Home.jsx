6b53723b970f9bbafc89f89e83ae8a7f79fa7627
aayushjoshi244 <aayush09345@gmail.com>
2024-05-26 21:37:06 +0530

    removed empty line

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,7 +45,6 @@ const Home = () => {
   return (
     <section className="w-full h-screen relative">
       {/* <div className='absolute top-28 left-0 right-0 z-0 flex items-center justify-center'>
-        
       </div> */}
       <Canvas
         className={`w-full h-screen bg-transparent ${
