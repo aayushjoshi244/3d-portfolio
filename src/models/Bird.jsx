1a5887e66db84dfdb8ca773482bd5b5e725f68e8
aayushjoshi244 <aayush09345@gmail.com>
2024-05-31 00:20:22 +0530

    bird commit

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import {useRef, useEffect} from 'react'
 import birdScene from '../assets/3d/bird.glb'
-import { useGLTF } from '@react-three/drei'
+import { useAnimations, useGLTF } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber';
 
 const Bird = () => {
     const { scene, animations } = useGLTF(birdScene);
+    const birdRef = useRef();
+    const {actions} = useAnimations(animations, birdRef);
+
+    useEffect(() => {
+        actions['Take 001'].play();
+    },[]);
+
+    useFrame((_, delta) => {
+      // update the Y position simulate the flight moving in a sin wave
+      birdRef.current.rotation.y = Math.sin(delta * 1.5) * 0.5;
+    })
+
+
   return (
-   <mesh position={[-5,2,1]} scale={[0.003,0.003,0.003]}>
+   <mesh position={[-5,2,1]} 
+        scale={[0.003,0.003,0.003]}
+        ref={birdRef}
+    >
         <primitive object={scene}/>
    </mesh>
   )
