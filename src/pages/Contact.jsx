533511088631f779468ec5c9ce529707401173a6
aayushjoshi244 <aayush09345@gmail.com>
2024-06-20 18:33:12 +0530

    introduction of fox

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,10 @@
 import React, { useRef, useState } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({ name: "", email: "", message: "" });
-  const [isLoading, setIsLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = ({ target: { name, value } }) => {
     setForm({ ...form, [name]: value });
@@ -12,28 +12,32 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setIsLoading(true);
+    setLoading(true);
 
-    emailjs.sendForm(
-      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
-      {
-        from_name: form.name,
-        to_name: "Aayush Joshi",
-        from_email: form.email,
-        to_email: 'aayush09345@gmail.com',
-        message: form.message,
-      },
-      import.meta.env.VITE_APP_EMAILJS_PUBLIC_Key,
-    ).then(() => {
-      setIsLoading(false);
-      // TODO: Show Success message
-      // TODO: Hide an alert
-    }).catch((error) => {
-      setIsLoading(false);
-      console.log(error);
-      // TODO: Show error message
-    });
+    console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Aayush Joshi",
+          from_email: form.email,
+          to_email: "aayush09345@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_Key,
+      )
+      .then(() => {
+        setLoading(false);
+        // TODO: Show Success message
+        // TODO: Hide an alert
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error);
+        // TODO: Show error message
+      });
   };
 
   const handleFocus = () => {};
@@ -43,7 +47,10 @@ const Contact = () => {
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
-        <form className="w-full flex flex-col gap-7 mt-14" onSubmit={handleSubmit}>
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
+        >
           <label className="text-black-500 font-semibold">
             Name
             <input
@@ -89,11 +96,11 @@ const Contact = () => {
           <button
             type="submit"
             className="btn"
-            disabled={isLoading}
+            disabled={loading}
             onFocus={handleFocus}
             onBlur={handleBlur}
           >
-            {isLoading ? 'Sending...' : 'Send Message'}
+            {loading ? "Sending..." : "Send Message"}
           </button>
         </form>
       </div>
